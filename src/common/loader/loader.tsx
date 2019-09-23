e61b8d2b36d29c7de038c3653951fb251e3be5f9
Dhiraj <admin@admins-MacBook-Pro.local>
2019-09-22 23:42:10 -0400

    loader comments

--- src/common/loader/loader.tsx
+++ src/common/loader/loader.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+// import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
 
 interface Styles {
     loader : string
 }
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    loader: {
-      margin: "auto",
-      display:"block",
-      width:"30%",
-      marginTop:"8%"
-    }
-  }),
-);
+// const useStyles = makeStyles((theme: Theme) =>
+//   createStyles({
+//     loader: {
+//       margin: "auto",
+//       display:"block",
+//       width:"30%",
+//       marginTop:"8%"
+//     }
+//   }),
+// );
 
 export default function Loader() {
-  const classes:Styles = useStyles();
-
   return (
     <div>
       ...Loading</div>
